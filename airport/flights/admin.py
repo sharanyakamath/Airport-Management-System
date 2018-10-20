7bf346e654044fff100d82056ad05cf9a53bc3a5
sharanyakamath <sherrykamath@gmail.com>
2018-10-20 15:44:20 +0530

    registered models in admin.py

--- airport/flights/admin.py
+++ airport/flights/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, Passenger, Flight, Security, Staff
+
+admin.site.register(User)
+admin.site.register(Passenger)
+admin.site.register(Flight)
+admin.site.register(Security)
+admin.site.register(Staff)
